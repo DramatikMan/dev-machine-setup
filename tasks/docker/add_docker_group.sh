8cc2ddad1c8cf1ac97d2c500cb8cfe58f8ca2d12
Sergey Pavlov <self@sergeypavlov.dev>
2022-01-09 17:47:40 +0300

    Update scripts

--- tasks/docker/add_docker_group.sh
+++ tasks/docker/add_docker_group.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-echo_task "Adding docker user group"
+echo_task "Resolving access"
 
-docker_group=$(getent group docker)  
+user_in_docker_group=$(getent group docker | grep -o $USER)  
 
 if [[ $? != 0 ]]; then
-    out=$(
-        sudo groupadd docker \
-        && sudo usermod -aG docker $USER \
-            2>&1 > /dev/null
-    )
+    echo_action "Creating a group if it does not exist"
+    group_exists=$(getent group docker)
+
+    if [[ $? != 0 ]]; then
+        out=$(sudo groupadd docker 2>&1 > /dev/null)
+    fi
+
+    catch_error
+
+    echo_action "Adding the current user to the group"
+    out=$(sudo usermod -aG docker $USER 2>&1 > /dev/null)
     catch_error
 else
     echo_ok "Already exists"
